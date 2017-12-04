57eae816b6beefe210131faf985ceb5d861950b3
Gabriel Csapo <gabecsapo@gmail.com>
2017-12-03 17:41:52 -0800

    makes authenticate more flexible (#30)
    * makes authenticate more flexible
    - [BREAKING] changes the interface for authentication to make it more flexible
    - when error is sent back to client ensure error is string
    
    * updates readme and example
    
    * adds README notice

--- example/index.js
+++ example/index.js
@@ -4,32 +4,39 @@ const Server = require('../');
 
 const port = process.env.PORT || 7005;
 
-const git = new Server(path.normalize(path.resolve(__dirname, 'tmp')), {
+const repos = new Server(path.normalize(path.resolve(__dirname, 'tmp')), {
     autoCreate: true,
-    authenticate: (type, repo, username, password, next) => {
-      console.log(type, repo, username, password); // eslint-disable-line
-      next();
+    authenticate: (type, repo, user, next) => {
+      console.log(type, repo); // eslint-disable-line
+      if(type == 'upload') {
+        user((username, password) => {
+          console.log(username, password); // eslint-disable-line
+          next();
+        });
+      } else {
+        next();
+      }
     }
 });
 
-git.on('push', (push) => {
+repos.on('push', (push) => {
     console.log(`push ${push.repo} / ${push.commit} ( ${push.branch} )`); // eslint-disable-line
-    git.list((err, result) => {
+    repos.list((err, result) => {
         console.log(result); // eslint-disable-line
     });
     push.accept();
 });
 
-git.on('fetch', (fetch) => {
+repos.on('fetch', (fetch) => {
     console.log('username', fetch.username); // eslint-disable-line
     console.log('fetch ' + fetch.repo + '/' + fetch.commit); // eslint-disable-line
     fetch.accept();
 });
 
-git.listen(port, (error) => {
+repos.listen(port, (error) => {
     if(error) return console.error(`failed to start git-server because of error ${error}`); // eslint-disable-line
     console.log(`node-git-server running at http://localhost:${port}`); // eslint-disable-line
-    git.list((err, result) => {
+    repos.list((err, result) => {
         if (!result) {
             console.log("No repositories available..."); // eslint-disable-line
         } else {
